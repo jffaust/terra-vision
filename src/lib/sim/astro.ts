cfd98038a2e95419b07beabb1aa95365c4e0b92c
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2022-11-02 15:47:36 -0400

    Use HelioVector for true positions

--- src/lib/sim/astro.ts
+++ src/lib/sim/astro.ts
@@ -12,18 +12,11 @@ export const astroEarthOrbit = derived(simStartDate, calculateEarthOrbit);
 
 function calculateProperties(date: Date): AstroSimData {
 
-    const ai = astro.RotationAxis(astro.Body.Earth, date);
-    console.log(ai);
-    console.log(astro.Ecliptic(ai.north))
+    // const ai = astro.RotationAxis(astro.Body.Earth, date);
+    // console.log(ai);
+    // console.log(astro.Ecliptic(ai.north))
 
-    // Use GeoVector instead of HelioVector to have the position corrected for light travel time and aberration
-    const gv = astro.GeoVector(astro.Body.Sun, date, true);
-    // Transform to ecliptic coordinates so the orbit appears on the horizontal plane in the scene
-    const ec = astro.Ecliptic(gv).vec;
-    // Sun will be at the origin so we inverse the vector to get the Earth's position relative to 
-    const pos = new astro.Vector(-ec.x, -ec.y, -ec.z, ec.t)
-
-    return { earth: { pos } };
+    return { earth: { pos: astro.HelioVector(astro.Body.Earth, date) } };
 }
 
 // calculates the orbital points for 6 months before the given date and 
@@ -40,11 +33,7 @@ function calculateEarthOrbit(date: Date): astro.Vector[] {
     let nextDate = firstDate;
     for (let i = 0; i <= segments; i++) {
         nextDate = new Date(nextDate.getTime() + segmentDelta);
-        const gv = astro.GeoVector(astro.Body.Sun, nextDate, true);
-        const pos = astro.Ecliptic(gv).vec;
-        // Inverse the vector to get the Earth's position relative to the sun
-        const helioPos = new astro.Vector(-pos.x, -pos.y, -pos.z, pos.t)
-        points.push(helioPos)
+        points.push(astro.HelioVector(astro.Body.Earth, nextDate))
     }
 
     return points;

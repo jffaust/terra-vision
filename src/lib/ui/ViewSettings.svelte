320cb854223ac2ee747a7fcd5b2dd4be0abae2c7
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2022-12-15 14:13:58 -0500

    Change view with modal (#8)

--- src/lib/ui/ViewSettings.svelte
+++ src/lib/ui/ViewSettings.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { gridView } from '$lib/stores';
-	import type { Rect, View } from '$lib/types';
+	import { VizTypes, type Rect, type View } from '$lib/types';
 	import type { Direction } from './gridview';
 
 	export let id: string;
 
 	let showDropdown = false;
+	let changeViewModal: HTMLDialogElement;
 
 	function splitView(dir: Direction) {
 		$gridView.split(id, dir);
@@ -19,6 +20,17 @@
 		$gridView = $gridView;
 		showDropdown = false;
 	}
+
+	function changeView() {
+		showDropdown = false;
+		changeViewModal.show();
+	}
+
+	function changeViewTo(type: VizTypes) {
+		$gridView.changeView(id, type);
+		$gridView = $gridView;
+		changeViewModal.close();
+	}
 </script>
 
 <div class="settings">
@@ -27,13 +39,26 @@
 	</button>
 	{#if showDropdown}
 		<ul>
-			<li>Change view</li>
+			<li on:click={changeView}>Change view</li>
 			<li on:click={() => splitView('Horizontal')}>Split right</li>
 			<li on:click={() => splitView('Vertical')}>Split down</li>
 			<li on:click={closeView}>Close view</li>
 		</ul>
 	{/if}
 </div>
+
+<dialog bind:this={changeViewModal}>
+	<menu class="view-picker">
+		<li on:click={() => changeViewTo(VizTypes.DefaultSpace)}>
+			<h4>Space</h4>
+			<p>View the Earth in space</p>
+		</li>
+		<li on:click={() => changeViewTo(VizTypes.SkyView)}>
+			<h4>Sky</h4>
+			<p>View the sky from a specific coordinate on Earth</p>
+		</li>
+	</menu>
+</dialog>
 
 <style>
 	.settings {
@@ -80,5 +105,30 @@
 
 	.hide {
 		display: none;
+	}
+
+	dialog {
+		position: fixed;
+		transform: translate(-50%, -50%);
+		left: 50%;
+		top: 50%;
+		padding: 0;
+		border: none;
+		background: none;
+	}
+
+	.view-picker {
+		border-radius: 5px;
+		border: 5px solid var(--bg-dark);
+		background-color: var(--bg-dark);
+		padding-inline-start: 0px;
+		list-style: none;
+	}
+	.view-picker > li {
+		padding: 10px;
+		cursor: pointer;
+	}
+	.view-picker > li:hover {
+		background-color: var(--bg5-dark);
 	}
 </style>

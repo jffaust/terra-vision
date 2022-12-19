550716556fcd0c382ff5be3bbd575541c50ccbfa
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2022-12-19 15:17:41 -0500

    Complex validation with shaking

--- src/lib/ui/GPSModal.svelte
+++ src/lib/ui/GPSModal.svelte
@@ -1,32 +1,58 @@
 <script lang="ts">
 	import { simGPS } from '$lib/sim/sim';
-	import { updateSearchParams } from '$lib/utils';
+	import { updateSearchParams, validateCoordinates } from '$lib/utils';
 	import Modal from './Modal.svelte';
 
 	export let close: () => void;
 
 	let lat = $simGPS?.lat;
 	let lon = $simGPS?.lon;
+	let inputLat: HTMLInputElement;
+	let inputLon: HTMLInputElement;
 
 	function clear() {
 		lat = undefined;
 		lon = undefined;
-		$simGPS = null;
 	}
 
 	function submit() {
-		if (lat === undefined && lon === undefined) {
+		console.log(lat, lon);
+		if (lat == undefined && lon == undefined) {
 			updateSearchParams('lat', null, true);
 			updateSearchParams('lon', null, true);
-		} else if (lat && lon && lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180) {
-			$simGPS = {
-				lat,
-				lon
-			};
-			updateSearchParams('lat', lat.toString(), true);
-			updateSearchParams('lon', lon.toString(), true);
+			$simGPS = null;
+			close();
+		} else if (lat != undefined && lon == undefined) {
+			shakeInputEl(inputLon);
+		} else if (lon != undefined && lat == undefined) {
+			shakeInputEl(inputLat);
 		}
-		close();
+
+		if (lat != undefined && (lat < -90 || lat > 90)) {
+			shakeInputEl(inputLat);
+		}
+		if (lon != undefined && (lon < -180 || lon > 180)) {
+			shakeInputEl(inputLon);
+		}
+		if (lat != undefined && lon != undefined) {
+			if (lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180) {
+				$simGPS = {
+					lat,
+					lon
+				};
+				updateSearchParams('lat', lat.toString(), true);
+				updateSearchParams('lon', lon.toString(), true);
+
+				close();
+			}
+		}
+	}
+
+	function shakeInputEl(el: HTMLInputElement) {
+		el.classList.add('shake');
+		setTimeout(() => {
+			el.classList.remove('shake');
+		}, 500);
 	}
 
 	function useMyLocation() {
@@ -54,9 +80,25 @@
 		</p>
 		<div class="input-row">
 			<label for="lat">Lat :</label>
-			<input type="number" id="lat" bind:value={lat} />
+			<input
+				bind:this={inputLat}
+				type="number"
+				id="lat"
+				bind:value={lat}
+				step="0.000000000000001"
+				min={-90}
+				max={90}
+			/>
 			<label for="lon">Lon :</label>
-			<input type="number" id="lon" bind:value={lon} />
+			<input
+				bind:this={inputLon}
+				type="number"
+				id="lon"
+				bind:value={lon}
+				step="0.000000000000001"
+				min={-180}
+				max={180}
+			/>
 		</div>
 
 		<footer class="is-right">
@@ -100,8 +142,34 @@
 	/* Firefox */
 	input[type='number'] {
 		-moz-appearance: textfield;
-		padding: 2px 4px 0 4px;
-		width: 90px !important;
+		padding: 2px 4px 0 4px !important;
+		width: 150px !important;
+		border-width: 2px !important;
+	}
+
+	input[type='number']:focus,
+	input[type='number']:hover {
+		border-color: var(--color-lightGrey) !important;
+	}
+
+	input[type='number']:invalid {
+		border: 2px solid crimson !important;
+	}
+
+	:is(.shake) {
+		animation: shake 300ms !important;
+	}
+
+	@keyframes shake {
+		25% {
+			transform: translateX(4px);
+		}
+		50% {
+			transform: translateX(-4px);
+		}
+		75% {
+			transform: translateX(4px);
+		}
 	}
 
 	footer {

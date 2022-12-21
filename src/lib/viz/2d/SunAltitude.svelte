0d3bf034b8d2b5082f38ed8eaaad370af2439424
Jean-Frédéric Faust <jean.frederic.faust@gmail.com>
2022-12-21 13:43:02 -0500

    Trying ot make multiline work

--- src/lib/viz/2d/SunAltitude.svelte
+++ src/lib/viz/2d/SunAltitude.svelte
@@ -6,18 +6,72 @@
 	import Scatter from '$lib/gaphics/2d/Scatter.svelte';
 	import { scaleTime } from 'd3-scale';
 	import { astroSkySim } from '$lib/sim/astro';
+	import CurvedLine from '$lib/gaphics/2d/CurvedLine.svelte';
+	import dateFormat from 'dateformat';
+	import MultiLine from '$lib/gaphics/2d/MultiLine.svelte';
 
 	interface DataPoint {
-		x: number;
-		y: number;
+		x: number; // number of seconds since midnight
+		y: number; // positive altitude of the sun
+		date: string;
 	}
 
-	// Define some data
-	$: points = calcDataPoints($simCurrentDate, $astroSkySim.altitude);
+	interface Series {
+		date: string;
+		values: DataPoint[];
+	}
 
-	function calcDataPoints(date: Date, altitude: number): DataPoint[] {
-		if (altitude < 0) return [];
-		else return [{ x: getTimeInSeconds(date), y: altitude }];
+	const lineTimeGranularity = 60 * 5; // 5 minutes
+
+	// Define some data
+	// $: points = calcDataPoints($simCurrentDate, $astroSkySim.altitude);
+	$: data = calcDataPoints2($simCurrentDate, $astroSkySim.altitude);
+
+	// function calcDataPoints(date: Date, altitude: number): DataPoint[] {
+	// 	if (altitude < 0) return [];
+	// 	else return [{ x: getTimeInSeconds(date), y: altitude }];
+	// }
+
+	function calcDataPoints2(date: Date, altitude: number): Series[] {
+		const currentDateKey = formatDateKey(date);
+
+		if (!data) {
+			data = [];
+		}
+
+		let series = findSeries(date);
+		if (!series) {
+			series = { date: currentDateKey, values: [] };
+			data.push(series);
+		}
+
+		if (altitude >= 0) {
+			let append = false;
+			const currentTime = getTimeInSeconds(date);
+
+			if (series.values.length == 0) {
+				append = true;
+			} else {
+				const lastTime = series.values[data.length - 1].x;
+				if (currentTime - lastTime > lineTimeGranularity) {
+					append = true;
+				}
+			}
+
+			if (append) {
+				series.values.push({ x: currentTime, y: altitude, date: currentDateKey });
+			}
+		}
+
+		return data;
+	}
+
+	function findSeries(date: Date): Series | undefined {
+		return data.find((s) => s.date === formatDateKey(date));
+	}
+
+	function formatDateKey(date: Date): string {
+		return dateFormat(date, 'yyyy-m-d');
 	}
 
 	function getTimeInSeconds(d: Date): number {
@@ -35,9 +89,10 @@
 	{:else}
 		<div class="chart-container">
 			<LayerCake
-				data={points}
+				{data}
 				x="x"
 				y="y"
+				z="date"
 				xDomain={[0, 24 * 60 * 60]}
 				yDomain={[0, 100]}
 				xScale={scaleTime()}
@@ -49,7 +104,8 @@
 						formatTick={formatTimeTick}
 					/>
 					<AxisY />
-					<Scatter fill={'yellow'} r={3} />
+					<MultiLine />
+					<!-- <Scatter fill={'yellow'} r={3} /> -->
 				</Svg>
 			</LayerCake>
 		</div>
